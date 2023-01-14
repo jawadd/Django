77d6e75377466e1b7ce827ade31d9c192f5a352b
Jawad Hussain <jawadduet@gmail.com>
2023-01-14 19:37:25 +0500

    Added Customer and Customer model classes

--- store/models.py
+++ store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    # sku = models.CharField(max_length=10, primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(255)
+    birth_date = models.DateField(null=True, blank=True)
