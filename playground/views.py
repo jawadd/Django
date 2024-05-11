4cea345f257cd99b66d9f98ca1c23a848c96e516
Jawad Hussain <jawadduet@gmail.com>
2024-05-11 21:57:25 +0500

    refactor

--- playground/views.py
+++ playground/views.py
@@ -10,33 +10,48 @@ from tags.models import TaggedItem
 
 
 def hello(request):
-    # does not require try and catch , as it will
-    # return result or none
-    # the .exists() return true or false
-    # product = Product.objects.filter(pk=0).first()
+    query_set = Product.objects.all()
+    return render(request, "hello.html", {'products': query_set})
 
-    # product = Product.objects.filter(unit_aprice__range=(20, 30))
-    # queryset = Product.objects.filter(unit_aprice__range=(20, 30))
-    # queryset = Product.objects.filter(title__icontains='coffee')
-    # combining conditions
-    # queryset = Product.objects.filter(inventory__lt=10, unit_aprice__lt=20)
-    # or we can also write
-    # queryset = Product.objects.filter(
-    #     inventory__lt=10).filter(unit_aprice__lt=30)
+   #  product=Product.objects.get(pk=1)
+   # the pk is auto translated into the required pk of the table by django
+   # for the above line we weill require try and catch blocks in order to handle object does not exist
+   # cases and we will require from django.core.exceptions import ObjectDoesNotExist
+   # then
+   # try:
+   #     product=Product.objects.get(pk=1)
+   # except ObjectDoesNotExist:
+   #     pass
+   # In Comparision exists=Product.objects.filter(pk=0).first does not require try and catch , as it will
+   # return result or none
+   # the .exists() return true or false
+   # product = Product.objects.filter(pk=0).first()
 
-    # for using the OR we need to import Q as above and modify the queryset accordingly
-    # queryset = Product.objects.filter(
-    # Q(inventory__lt=20) | Q(unit_price__lt=20))
-    # to compare two different fields
-    queryset = Product.objects.filter(inventory=F('unit_price'))
-    # we can also reference field of a related table
-    #  queryset = Product.objects.filter(inventory=F('collection__id'))
-    # Product.objects.order_by('title') , sorts by ascending
-    #   #Product.objects.order_by('-title') , sorts by descending
-    # .earliest() ascending, .latest('unit_price') descending
-    # below two are the same , they return single result and not a queryset
-    # product = Product.objects.latest('unit_price')
-    #  product= Product.objects.order_by('unit_price')[0]
-    TaggedItem.objects.get_tags_for(Product, 1)
+    # Filtering Results google queryset api django and then field lookups
+#     queryset=Product.objects.filter(unit_price__lt=20)
+# #    queryset = Product.objects.filter(unit_price__range=(20, 30))
+#     return render(request, "filtering.html", {'products': list(queryset)})
+   # queryset = Product.objects.filter(unit_aprice__range=(20, 30))
+#    case insensetive search
+   # queryset = Product.objects.filter(title__icontains='coffee')
+   # queryset = Product.objects.filter(title__startswith, endswith etc='coffee')
+   # combining conditions
+   # queryset = Product.objects.filter(inventory__lt=10, unit_aprice__lt=20)
+   # or we can also write
+   # queryset = Product.objects.filter(
+   #     inventory__lt=10).filter(unit_aprice__lt=30)
 
-    return render(request, 'hello.html', {'name': 'Jawad', 'tags': list(queryset)})
+   # for using the OR we need to import Q as above and modify the queryset accordingly
+   # queryset = Product.objects.filter(
+   # Q(inventory__lt=20) | Q(unit_price__lt=20))
+   # to compare two different fields
+   # queryset = Product.objects.filter(inventory=F('unit_price'))
+   # we can also reference field of a related table
+   #  queryset = Product.objects.filter(inventory=F('collection__id'))
+   # Product.objects.order_by('title') , sorts by ascending
+   #   #Product.objects.order_by('-title') , sorts by descending
+   # .earliest() ascending, .latest('unit_price') descending
+   # below two are the same , they return single result and not a queryset
+   # product = Product.objects.latest('unit_price')
+   #  product= Product.objects.order_by('unit_price')[0]
+   # TaggedItem.objects.get_tags_for(Product, 1)
